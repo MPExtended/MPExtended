1361709a18df0b50f5e09f4553190897bc9a04b5
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-08-01 16:07:28 +0000

    - Rename projects to new structure (Client/Server/SingleSeat) - Move everything out the MPExtended dir

---
+++ Trunk/ServiceHosts/MPExtended.ServiceHosts.Server/CoreService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace MPExtended.ServiceHosts.Server
+{
+    public partial class CoreService : ServiceBase
+    {
+        public CoreService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
