a1d82622179a979e47bafd7f5692a8b139629a65
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-08-06 19:03:22 +0000

    Add WCFUtil for misc WCF-related functions

--- Trunk/Libraries/MPExtended.Libraries.ServiceLib/MPEServices.cs
+++ Trunk/Libraries/MPExtended.Libraries.ServiceLib/MPEServices.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿#region Copyright (C) 2011 MPExtended
+// Copyright (C) 2011 MPExtended Developers, http://mpextended.codeplex.com/
+// 
+// MPExtended is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MPExtended is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MPExtended. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,17 +39,9 @@ namespace MPExtended.Libraries.ServiceLib
             {
                 if (_mediaService == null || ((ICommunicationObject)_mediaService).State == CommunicationState.Faulted)
                 {
-                    try
-                    {
-                        _mediaService = ChannelFactory<IMediaAccessService>.CreateChannel(new NetNamedPipeBinding() { MaxReceivedMessageSize = 10000000 }, new EndpointAddress("net.pipe://localhost/MPExtended/MediaAccessService"));
-
-                    }
-                    catch (Exception ex)
-                    {
-
-                        Log.Error("Exception in ServiceInstance", ex);
-                    }
+                    _mediaService = ChannelFactory<IMediaAccessService>.CreateChannel(new NetNamedPipeBinding() { MaxReceivedMessageSize = 10000000 }, new EndpointAddress("net.pipe://localhost/MPExtended/MediaAccessService"));
                 }
+
                 return _mediaService;
             }
         }
@@ -45,7 +54,6 @@ namespace MPExtended.Libraries.ServiceLib
                 {
                     _tvService = ChannelFactory<ITVAccessService>.CreateChannel(new NetNamedPipeBinding() { MaxReceivedMessageSize = 10000000 }, new EndpointAddress("net.pipe://localhost/MPExtended/TVAccessService"));
                 }
-
                 return _tvService;
             }
         }
