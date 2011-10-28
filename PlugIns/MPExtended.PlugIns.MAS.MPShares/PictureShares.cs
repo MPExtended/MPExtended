bc1d6d9396f37ed0d0ff33479da30ee2d6615bfe
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-10-29 01:10:53 +0200

    Create separate libraries for movie, music and picture shares

---
+++ PlugIns/MPExtended.PlugIns.MAS.MPShares/PictureShares.cs
@@ -0,0 +1,39 @@
+﻿#region Copyright (C) 2011 MPExtended
+// Copyright (C) 2011 MPExtended Developers, http://mpextended.github.com/
+// 
+// MPExtended is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MPExtended is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MPExtended. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using MPExtended.Services.MediaAccessService.Interfaces;
+
+namespace MPExtended.PlugIns.MAS.MPShares
+{
+    [Export(typeof(IFileSystemLibrary))]
+    [ExportMetadata("Name", "MP Picture Shares")]
+    [ExportMetadata("Type", typeof(PictureShares))]
+    [ExportMetadata("Id", 11)]
+    public class PictureShares : ShareLibrary
+    {
+        [ImportingConstructor]
+        public PictureShares(IPluginData data) : base (data)
+        {
+            Sections = new string[] { "pictures" };
+        }
+    }
+}
