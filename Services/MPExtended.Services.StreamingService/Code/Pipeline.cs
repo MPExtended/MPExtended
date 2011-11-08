c0442c01c0846b731dc5a05192dcd23246e0a436
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-11-08 17:27:44 +0100

    Change indenting style in Pipeline.cs
    It still had the old indenting style from MPWebStream

--- Services/MPExtended.Services.StreamingService/Code/Pipeline.cs
+++ Services/MPExtended.Services.StreamingService/Code/Pipeline.cs
@@ -23,51 +23,61 @@ using System.Text;
 using MPExtended.Libraries.General;
 using MPExtended.Services.StreamingService.Units;
 
-namespace MPExtended.Services.StreamingService.Code {
-    internal class Pipeline {
+namespace MPExtended.Services.StreamingService.Code
+{
+    internal class Pipeline
+    {
         private Dictionary<int, IProcessingUnit> dataUnits = new Dictionary<int, IProcessingUnit>();
         private Dictionary<int, ILogProcessingUnit> logUnits = new Dictionary<int, ILogProcessingUnit>();
         public bool IsAssembled { get; private set; }
         public bool IsStarted { get; private set; }
         public bool IsStopped { get; private set; }
 
-        public Pipeline() {
+        public Pipeline()
+        {
             IsAssembled = false;
             IsStarted = false;
             IsStopped = false;
         }
 
-        public void AddDataUnit(IProcessingUnit process, int position) {
+        public void AddDataUnit(IProcessingUnit process, int position)
+        {
             dataUnits[position] = process;
             dataUnits[position].IsInputStreamConnected = false;
             dataUnits[position].IsDataStreamConnected = false;
             dataUnits[position].IsLogStreamConnected = false;
         }
 
-        public void AddLogUnit(ILogProcessingUnit process, int position) {
+        public void AddLogUnit(ILogProcessingUnit process, int position)
+        {
             logUnits[position] = process;
         }
 
-        public IProcessingUnit GetDataUnit(int position) {
+        public IProcessingUnit GetDataUnit(int position)
+        {
             if (!dataUnits.ContainsKey(position))
                 return null;
             return dataUnits[position];
         }
 
-        public ILogProcessingUnit GetLogUnit(int position) {
+        public ILogProcessingUnit GetLogUnit(int position)
+        {
             if (!logUnits.ContainsKey(position))
                 return null;
             return logUnits[position];
         }
 
-        public bool Assemble() {
+        public bool Assemble()
+        {
             IsAssembled = true;
-            Dictionary<int, int> dataConnections = new Dictionary<int,int>();
-            Dictionary<int, int> logConnections = new Dictionary<int,int>();
+            Dictionary<int, int> dataConnections = new Dictionary<int, int>();
+            Dictionary<int, int> logConnections = new Dictionary<int, int>();
 
             int lastKey = -1;
-            foreach (int i in dataUnits.Keys.OrderBy(k => k)) {
-                if (dataUnits.ContainsKey(lastKey)) {
+            foreach (int i in dataUnits.Keys.OrderBy(k => k))
+            {
+                if (dataUnits.ContainsKey(lastKey))
+                {
                     dataConnections[lastKey] = i;
                     dataUnits[i].IsInputStreamConnected = true;
                     dataUnits[lastKey].IsDataStreamConnected = true;
@@ -76,9 +86,11 @@ namespace MPExtended.Services.StreamingService.Code {
                 lastKey = i;
             }
 
-            foreach (int i in logUnits.Keys.OrderBy(k => k)) {
+            foreach (int i in logUnits.Keys.OrderBy(k => k))
+            {
                 int nr = dataUnits.Keys.Where(k => k < i).DefaultIfEmpty(-1).Max();
-                if (dataUnits.ContainsKey(nr)) {
+                if (dataUnits.ContainsKey(nr))
+                {
                     logConnections[i] = nr;
                     dataUnits[nr].IsLogStreamConnected = true;
                 }
@@ -98,17 +110,21 @@ namespace MPExtended.Services.StreamingService.Code {
             foreach (KeyValuePair<int, int> conn in logConnections)
                 Log.Info("   logconn {0} -> {1}", conn.Value, conn.Key);
 
-            foreach (int i in dataUnits.Keys.OrderBy(k => k)) {
-                if (!dataUnits[i].Setup()) {
+            foreach (int i in dataUnits.Keys.OrderBy(k => k))
+            {
+                if (!dataUnits[i].Setup())
+                {
                     // it failed, stop and break out
                     Log.Error("Setup of data unit {0} failed", i);
                     Stop(true);
                     return false;
-                }   
-                if(dataConnections.ContainsKey(i))
+                }
+                if (dataConnections.ContainsKey(i))
                     dataUnits[dataConnections[i]].InputStream = dataUnits[i].DataOutputStream;
             }
-            foreach (int i in logUnits.Keys.OrderBy(k => k)) {
+
+            foreach (int i in logUnits.Keys.OrderBy(k => k))
+            {
                 if (logConnections.ContainsKey(i))
                     logUnits[i].InputStream = dataUnits[logConnections[i]].LogOutputStream;
                 logUnits[i].Setup();
@@ -118,38 +134,49 @@ namespace MPExtended.Services.StreamingService.Code {
             return true;
         }
 
-        public bool Start() {
+        public bool Start()
+        {
             if (!IsAssembled)
                 Assemble();
             IsStarted = true;
 
-            foreach (int i in dataUnits.Keys.OrderBy(k => k)) {
+            foreach (int i in dataUnits.Keys.OrderBy(k => k))
+            {
                 Log.Info("Starting data unit {0}", i);
-                if (!dataUnits[i].Start()) {
+                if (!dataUnits[i].Start())
+                {
                     Log.Error("Starting data unit {0} failed", i);
                     Stop(true);
                     return false;
                 }
             }
+
             Log.Info("All data units started!");
             foreach (int i in logUnits.Keys.OrderBy(k => k))
+            {
                 logUnits[i].Start();
+            }
 
             return true;
         }
 
-        public bool Stop(bool force) {
+        public bool Stop(bool force)
+        {
             if (IsStopped)
                 return true;
             if (!IsStarted && !force)
                 Start();
 
-            foreach (int i in dataUnits.Keys.OrderBy(k => k)) {
+            foreach (int i in dataUnits.Keys.OrderBy(k => k))
+            {
                 Log.Info("Stopping data unit {0}", i);
                 dataUnits[i].Stop();
             }
+
             foreach (int i in logUnits.Keys.OrderBy(k => k))
+            {
                 logUnits[i].Stop();
+            }
 
             Log.Debug("Pipeline stopped");
             IsStopped = true;
@@ -161,19 +188,23 @@ namespace MPExtended.Services.StreamingService.Code {
             return Stop(false);
         }
 
-        public Stream GetFinalStream() {
+        public Stream GetFinalStream()
+        {
             if (!IsStarted)
                 return null;
 
             return dataUnits[dataUnits.Keys.Max()].DataOutputStream;
         }
 
-        public bool RunBlocking() {
+        public bool RunBlocking()
+        {
             if (!IsStarted)
                 Start();
 
-            foreach (int i in dataUnits.Keys.OrderBy(k => k)) {
-                if (dataUnits[i] is IBlockingProcessingUnit) {
+            foreach (int i in dataUnits.Keys.OrderBy(k => k))
+            {
+                if (dataUnits[i] is IBlockingProcessingUnit)
+                {
                     ((IBlockingProcessingUnit)dataUnits[i]).RunBlocking();
                     return true;
                 }
