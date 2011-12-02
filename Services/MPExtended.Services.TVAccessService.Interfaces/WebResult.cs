496c7ba265934c3d317ddf2735e2c8856c3ddfe5
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-12-02 21:36:32 +0100

    Implement operator overloads in WebResult

--- Services/MPExtended.Services.TVAccessService.Interfaces/WebResult.cs
+++ Services/MPExtended.Services.TVAccessService.Interfaces/WebResult.cs
@@ -18,6 +18,27 @@ namespace MPExtended.Services.TVAccessService.Interfaces
             Result = value;
         }
 
+        public override bool Equals(object obj)
+        {
+            WebResult r = obj is bool ? new WebResult((bool)obj) : obj as WebResult;
+            return (object)r != null && this.Result == r.Result;
+        }
+
+        public override int GetHashCode()
+        {
+            return Result ? 1 : 0;
+        }
+
+        public static bool operator ==(WebResult a, WebResult b)
+        {
+            return Object.ReferenceEquals(a, b) || (((object)a) != null && ((object)b) != null && a.Result == b.Result);
+        }
+
+        public static bool operator !=(WebResult a, WebResult b)
+        {
+            return !(a == b);
+        }
+
         public static implicit operator WebResult(bool value)
         {
             return new WebResult(value);
