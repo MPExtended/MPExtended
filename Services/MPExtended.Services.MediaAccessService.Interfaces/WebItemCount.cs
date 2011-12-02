bfc8e58f8ffc84f4f3196e484cd6a20ade44b34d
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-12-02 21:23:14 +0100

    Add operator overloading on WebItemCount (fixes #66)
    This makes it act like a native integer, allowing to compare them and perform
    arithmetic on them.

--- Services/MPExtended.Services.MediaAccessService.Interfaces/WebItemCount.cs
+++ Services/MPExtended.Services.MediaAccessService.Interfaces/WebItemCount.cs
@@ -9,14 +9,43 @@ namespace MPExtended.Services.MediaAccessService.Interfaces
     {
         public int Count { get; set; }
 
+        public WebItemCount()
+        {
+        }
+
+        public WebItemCount(int value)
+        {
+            this.Count = value;
+        }
+
         public override bool Equals(object obj)
         {
-            return (obj is Int32 && (Int32)obj == Count) || (obj is WebItemCount && (obj as WebItemCount).Count == this.Count);
+            return obj as WebItemCount != null && (obj as WebItemCount).Count == this.Count;
         }
 
         public override int GetHashCode()
         {
             return Count;
+        }
+
+        public static bool operator ==(WebItemCount a, WebItemCount b)
+        {
+            return a.Count == b.Count;
+        }
+
+        public static bool operator !=(WebItemCount a, WebItemCount b)
+        {
+            return a.Count != b.Count;
+        }
+
+        public static implicit operator int(WebItemCount count)
+        {
+            return count.Count;
+        }
+
+        public static implicit operator WebItemCount(int count)
+        {
+            return new WebItemCount(count);
         }
     }
 }
