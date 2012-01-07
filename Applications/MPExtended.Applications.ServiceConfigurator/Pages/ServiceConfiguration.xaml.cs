MPExtended/MPExtended

commit 68d354b2fcd982ced985e0ffb2d96b8a8713e906

Configurator: remove service not installed warning when running from source

Applications/MPExtended.Applications.ServiceConfigurator/Pages/ServiceConfiguration.xaml.cs
@@ -51,9 +51,12 @@ namespace MPExtended.Applications.ServiceConfigurator.Pages
             }
             catch (InvalidOperationException)
             {
-                mServiceController = null;
-                Log.Error("MPExtended Service not installed");
-                MessageBox.Show("MPExtended Service not installed. Please make sure your installation is correct.", "MPExtended", MessageBoxButton.OK, MessageBoxImage.Error);
+                if (Installation.GetFileLayoutType() != FileLayoutType.Source)
+                {
+                    mServiceController = null;
+                    Log.Error("MPExtended Service not installed");
+                    MessageBox.Show("MPExtended Service not installed. Please make sure your installation is correct.", "MPExtended", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
 
             // initialize watcher

commit e384ba4ea2cd69927a37bbdd402d5e77dc569a39

Configurator: do not try to access service when not installed
Fixes regression introduced in 68d354b

Applications/MPExtended.Applications.ServiceConfigurator/Pages/ServiceConfiguration.xaml.cs
@@ -51,9 +51,9 @@ namespace MPExtended.Applications.ServiceConfigurator.Pages
             }
             catch (InvalidOperationException)
             {
+                mServiceController = null;
                 if (Installation.GetFileLayoutType() != FileLayoutType.Source)
                 {
-                    mServiceController = null;
                     Log.Error("MPExtended Service not installed");
                     MessageBox.Show("MPExtended Service not installed. Please make sure your installation is correct.", "MPExtended", MessageBoxButton.OK, MessageBoxImage.Error);
                 }