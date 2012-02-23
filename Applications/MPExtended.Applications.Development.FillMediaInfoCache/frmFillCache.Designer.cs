49592e7403b13e2ff53ba68404591bbd38384160
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2012-02-23 23:28:23 +0100

    Add a testtool to fill the MediaInfo cache

---
+++ Applications/MPExtended.Applications.Development.FillMediaInfoCache/frmFillCache.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MPExtended.Applications.Development.FillMediaInfoCache
+{
+    partial class frmFillCache
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMovies = new System.Windows.Forms.Label();
+            this.lblEpisodes = new System.Windows.Forms.Label();
+            this.prgMovies = new System.Windows.Forms.ProgressBar();
+            this.prgEpisodes = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(16, 13);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(256, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblMovies
+            // 
+            this.lblMovies.AutoSize = true;
+            this.lblMovies.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovies.Location = new System.Drawing.Point(13, 55);
+            this.lblMovies.Name = "lblMovies";
+            this.lblMovies.Size = new System.Drawing.Size(90, 15);
+            this.lblMovies.TabIndex = 1;
+            this.lblMovies.Text = "Movies (8 / 12):";
+            // 
+            // lblEpisodes
+            // 
+            this.lblEpisodes.AutoSize = true;
+            this.lblEpisodes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEpisodes.Location = new System.Drawing.Point(13, 124);
+            this.lblEpisodes.Name = "lblEpisodes";
+            this.lblEpisodes.Size = new System.Drawing.Size(102, 15);
+            this.lblEpisodes.TabIndex = 2;
+            this.lblEpisodes.Text = "Episodes (8 / 12):";
+            // 
+            // prgMovies
+            // 
+            this.prgMovies.Location = new System.Drawing.Point(16, 82);
+            this.prgMovies.Name = "prgMovies";
+            this.prgMovies.Size = new System.Drawing.Size(256, 23);
+            this.prgMovies.TabIndex = 3;
+            // 
+            // prgEpisodes
+            // 
+            this.prgEpisodes.Location = new System.Drawing.Point(16, 153);
+            this.prgEpisodes.Name = "prgEpisodes";
+            this.prgEpisodes.Size = new System.Drawing.Size(256, 23);
+            this.prgEpisodes.TabIndex = 4;
+            // 
+            // frmFillCache
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 189);
+            this.Controls.Add(this.prgEpisodes);
+            this.Controls.Add(this.prgMovies);
+            this.Controls.Add(this.lblEpisodes);
+            this.Controls.Add(this.lblMovies);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmFillCache";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmFillCache_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMovies;
+        private System.Windows.Forms.Label lblEpisodes;
+        private System.Windows.Forms.ProgressBar prgMovies;
+        private System.Windows.Forms.ProgressBar prgEpisodes;
+
+    }
+}
+
---
+++ Applications/MPExtended.Applications.Development.FillMediaInfoCache/frmFillCache.cs
@@ -0,0 +1,140 @@
+﻿#region Copyright (C) 2012 MPExtended
+// Copyright (C) 2012 MPExtended Developers, http://mpextended.github.com/
+// 
+// MPExtended is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MPExtended is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MPExtended. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MPExtended.Libraries.Client;
+using MPExtended.Services.StreamingService.Interfaces;
+
+namespace MPExtended.Applications.Development.FillMediaInfoCache
+{
+    public partial class frmFillCache : Form
+    {
+        private BackgroundWorker bwMovies;
+        private BackgroundWorker bwEpisodes;
+        private bool isRunning = false;
+
+        private int movieCount;
+        private int episodeCount;
+
+        public frmFillCache()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFillCache_Load(object sender, EventArgs e)
+        {
+            movieCount = MPEServices.MAS.GetMovieCount(null).Count;
+            episodeCount = MPEServices.MAS.GetTVEpisodeCount(null).Count;
+
+            lblMovies.Text = "Movies (0 / " + movieCount + "):";
+            prgMovies.Maximum = movieCount;
+            lblEpisodes.Text = "Episodes (0 / " + episodeCount + "):";
+            prgEpisodes.Maximum = episodeCount;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                bwMovies.CancelAsync();
+                bwEpisodes.CancelAsync();
+
+                isRunning = false;
+                btnStart.Text = "Start";
+            }
+            else
+            {
+                bwMovies = new BackgroundWorker();
+                bwMovies.WorkerReportsProgress = true;
+                bwMovies.WorkerSupportsCancellation = true;
+                bwMovies.ProgressChanged += delegate(object bwSender, ProgressChangedEventArgs bwE)
+                {
+                    lblMovies.Text = "Movies (" + bwE.UserState + " / " + movieCount + "):";
+                    prgMovies.Value = (int)bwE.UserState;
+                };
+                bwMovies.DoWork += delegate(object bwSender, DoWorkEventArgs bwE)
+                {
+                    int j = 0;
+                    BackgroundWorker worker = (BackgroundWorker)bwSender;
+                    for (int i = 0; i < movieCount; i += 100)
+                    {
+                        var movies = MPEServices.MAS.GetMoviesBasicByRange(null, i, i + 100);
+                        foreach (var movie in movies)
+                        {
+                            MPEServices.MASStreamControl.GetMediaInfo(WebStreamMediaType.Movie, movie.PID, movie.Id);
+                            worker.ReportProgress(5, ++j);
+                            if (worker.CancellationPending)
+                            {
+                                bwE.Cancel = true;
+                                goto outMovies;
+                            }
+                        }
+                    }
+outMovies:
+                    ;
+                };
+                bwMovies.RunWorkerAsync();
+
+                bwEpisodes = new BackgroundWorker();
+                bwEpisodes.WorkerReportsProgress = true;
+                bwEpisodes.WorkerSupportsCancellation = true;
+                bwEpisodes.ProgressChanged += delegate(object bwSender, ProgressChangedEventArgs bwE)
+                {
+                    lblEpisodes.Text = "Episodes (" + bwE.UserState + " / " + episodeCount + "):";
+                    prgEpisodes.Value = (int)bwE.UserState;
+                };
+                bwEpisodes.DoWork += delegate(object bwSender, DoWorkEventArgs bwE)
+                {
+                    int j = 0;
+                    BackgroundWorker worker = (BackgroundWorker)bwSender;
+                    for (int i = 0; i < episodeCount; i += 100)
+                    {
+                        var episodes = MPEServices.MAS.GetTVEpisodesBasicByRange(null, i, i + 100);
+                        foreach (var episode in episodes)
+                        {
+                            MPEServices.MASStreamControl.GetMediaInfo(WebStreamMediaType.TVEpisode, episode.PID, episode.Id);
+                            worker.ReportProgress(5, ++j);
+                            if (worker.CancellationPending)
+                            {
+                                bwE.Cancel = true;
+                                goto outEpisodes;
+                            }
+                        }
+                    }
+outEpisodes:
+                    ;
+                };
+                bwEpisodes.RunWorkerAsync();
+
+                btnStart.Text = "Stop";
+                isRunning = true;
+            }
+        }
+
+        void bwMovies_DoWork(object sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
