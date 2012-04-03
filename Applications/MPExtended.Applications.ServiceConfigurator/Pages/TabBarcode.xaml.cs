MPExtended/MPExtended

commit 888c8bb2e49fa3fbdea0f7d7bc58be6aa29b81f9

Rewrite the service detection and hosting infrastructure

Applications/MPExtended.Applications.ServiceConfigurator/Pages/TabBarcode.xaml.cs
@@ -98,16 +98,14 @@ namespace MPExtended.Applications.ServiceConfigurator.Pages
                 {
                     var srvdesc = new ServiceDescription()
                     {
-                        Name = srv.ServiceName.ToString(),
+                        Name = srv.Service.ToString(),
                         Port = srv.Port
                     };
 
                     if (auth != null)
                     {
-                        string usernameOut, passwordOut;
-                        srv.GetUsernameAndPassword(auth, out usernameOut, out passwordOut);
-                        srvdesc.User = usernameOut;
-                        srvdesc.Password = passwordOut;
+                        srvdesc.User = auth.Username;
+                        srvdesc.Password = auth.GetPassword();
                     }
 
                     desc.Services.Add(srvdesc);

Libraries/MPExtended.Libraries.Service/Installation.cs
@@ -23,6 +23,7 @@ using System.Reflection;
 using System.Text;
 using System.Xml.Linq;
 using Microsoft.Win32;
+using MPExtended.Libraries.Service.Hosting;
 using MPExtended.Libraries.Service.Util;
 
 namespace MPExtended.Libraries.Service
@@ -48,8 +49,17 @@ namespace MPExtended.Libraries.Service
         Installed
     }
 
+    public class ServiceConfiguration
+    {
+        public MPExtendedService Service { get; set; }
+        public string ZeroconfType { get; set; }
+        public int Port { get; set; }
+    }
+
     public static class Installation
     {
+        private static List<ServiceAssemblyAttribute> installedServices;
+
         public static FileLayoutType GetFileLayoutType()
         {
             // Source distribution: search for a parent directory with the GlobalVersion.cs file
@@ -171,30 +181,38 @@ namespace MPExtended.Libraries.Service
             return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "MPExtended", "Logs");
         }
 
-        public static List<Hosting.Service> GetInstalledServices()
+        internal static List<ServiceAssemblyAttribute> GetAvailableServices()
         {
-            var allServices = new List<Hosting.Service>()
+            if (installedServices == null)
             {
-                new Hosting.Service(MPExtendedService.MediaAccessService, "MPExtended.Services.MediaAccessService", "MediaAccessService", "_mpextended-mas._tcp"),
-                new Hosting.Service(MPExtendedService.TVAccessService, "MPExtended.Services.TVAccessService", "TVAccessService", "_mpextended-tas._tcp"),
-                new Hosting.Service(MPExtendedService.StreamingService, "MPExtended.Services.StreamingService", "StreamingService", "_mpextended-wss._tcp"),
-                new Hosting.Service(MPExtendedService.UserSessionService, "MPExtended.Services.UserSessionService", "UserSessionProxyService", "_mpextended-uss._tcp"),
-                new Hosting.WifiRemoteService()
-            };
+                string directory = Installation.GetFileLayoutType() == FileLayoutType.Installed ?
+                    Installation.GetInstallDirectory(MPExtendedProduct.Service) :
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            string[] disabled =
-                XElement.Load(Configuration.GetPath("Services.xml"))
-                .Element("disabledServices")
-                .Elements("service")
-                .Select(x => x.Value)
-                .ToArray();
+                var files = Directory.GetFiles(directory, "MPExtended.Services.*.dll");
 
-            return allServices.Where(x => x.IsInstalled && !disabled.Contains(x.Assembly)).ToList();
+                installedServices = files
+                    .Select(path => Assembly.LoadFrom(path))
+                    .SelectMany(asm => asm.GetCustomAttributes(typeof(ServiceAssemblyAttribute), false).Cast<ServiceAssemblyAttribute>())
+                    .ToList();
+            }
+
+            return installedServices;
+        }
+
+        public static List<ServiceConfiguration> GetInstalledServices()
+        {
+            return GetAvailableServices().Select(x => new ServiceConfiguration()
+            {
+                Port = Configuration.Services.Port,
+                Service = x.Service,
+                ZeroconfType = x.ZeroconfType
+            }).ToList();
         }
 
         public static bool IsServiceInstalled(MPExtendedService srv)
         {
-            return GetInstalledServices().Any(x => x.ServiceName == srv);
+            return GetInstalledServices().Any(x => x.Service == srv);
         }
     }
 }

Libraries/MPExtended.Libraries.Service/MPExtended.Libraries.Service.csproj
@@ -74,13 +74,12 @@
     <Compile Include="ConfigurationContracts\Services.cs" />
     <Compile Include="ConfigurationContracts\Streaming.cs" />
     <Compile Include="ConfigurationContracts\WebMediaPortalHosting.cs" />
+    <Compile Include="Hosting\ServiceAssemblyAttribute.cs" />
     <Compile Include="WCF\CustomQueryStringConverter.cs" />
     <Compile Include="Hosting\BaseAddresses.cs" />
     <Compile Include="Hosting\LogRotation.cs" />
     <Compile Include="Hosting\MPExtendedHost.cs" />
-    <Compile Include="Hosting\Service.cs" />
     <Compile Include="Hosting\WCFHost.cs" />
-    <Compile Include="Hosting\WifiRemoteService.cs" />
     <Compile Include="Hosting\Zeroconf.cs" />
     <Compile Include="Util\Impersonator.cs" />
     <Compile Include="Installation.cs" />

Config/Debug/Services.xml
@@ -23,17 +23,6 @@
   <port>4322</port>
   <enableIPv6>false</enableIPv6>
 
-  <disabledServices>
-    <!--
-      You can disable individual services (for debugging) here. Enter the assembly name in a <service> tag:
-      
-        <service>MPExtended.Services.MediaAccessService</service>
-        <service>MPExtended.Services.TVAccessService</service>
-        <service>MPExtended.Services.StreamingService</service>
-        <service>MPExtended.Services.UserSessionService</service>
-    -->
-  </disabledServices>
-
   <!-- If you've have multiseat setup you can specify different hosts for the other host here. This is picked up by WSS (but it doesn't work) and by WebMP. -->
   <connections>
     <mas>auto://127.0.0.1:4322</mas>

Config/Release/Services.xml
@@ -22,17 +22,6 @@
   
   <port>4322</port>
   <enableIPv6>false</enableIPv6>
-  
-  <disabledServices>
-    <!--
-      You can disable individual services (for debugging) here. Enter the assembly name in a <service> tag:
-      
-        <service>MPExtended.Services.MediaAccessService</service>
-        <service>MPExtended.Services.TVAccessService</service>
-        <service>MPExtended.Services.StreamingService</service>
-        <service>MPExtended.Services.UserSessionService</service>
-    -->
-  </disabledServices>
 
   <!-- If you've have multiseat setup you can specify different hosts for the other host here. This is picked up by WebMP and in the future also by WSS -->
   <connections>

Libraries/MPExtended.Libraries.Service/Hosting/MPExtendedHost.cs
@@ -19,6 +19,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace MPExtended.Libraries.Service.Hosting
 {
@@ -26,20 +27,12 @@ namespace MPExtended.Libraries.Service.Hosting
     {
         private WCFHost wcf;
         private Zeroconf zeroconf;
-        private List<Type> serviceTypes;
 
         public MPExtendedHost()
         {
-            System.Threading.Thread.CurrentThread.Name = "HostThread";
+            Thread.CurrentThread.Name = "HostThread";
             wcf = new WCFHost();
             zeroconf = new Zeroconf();
-        }
-
-        public MPExtendedHost(List<Type> types)
-            : this()
-        {
-            serviceTypes = types;
-            wcf.SetTypes(types);
         }
 
         public bool Open()
@@ -62,8 +55,21 @@ namespace MPExtended.Libraries.Service.Hosting
                     }
                 };
 
-                // perform the actual start
-                wcf.Start(Installation.GetInstalledServices().Where(x => x.HostAsWCF).ToList());
+                // start the WCF services
+                wcf.Start(Installation.GetAvailableServices().Where(x => x.WCFType != null));
+
+                // start the background threads
+                var services = Installation.GetAvailableServices().Where(x => x.BackgroundThread != null);
+                foreach (var service in services)
+                {
+                    string name = service.Service.ToString() + "Background";
+                    ThreadManager.Start(name, delegate()
+                    {
+                        service.BackgroundThread.Invoke();
+                    });
+                }
+
+                // do the zeroconf publish
                 ThreadManager.Start("Zeroconf", delegate()
                 {
                     try
@@ -75,7 +81,6 @@ namespace MPExtended.Libraries.Service.Hosting
                         Log.Error("Zeroconf publish failed", ex);
                     }
                 });
-                Log.Debug("Opened MPExtended ServiceHost");
                 return true;
             }
             catch (Exception ex)

Libraries/MPExtended.Libraries.Service/Hosting/Service.cs
@@ -1,93 +1,0 @@
-﻿#region Copyright (C) 2011-2012 MPExtended
-// Copyright (C) 2011-2012 MPExtended Developers, http://mpextended.github.com/
-// 
-// MPExtended is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-// 
-// MPExtended is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with MPExtended. If not, see <http://www.gnu.org/licenses/>.
-#endregion
-
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using Microsoft.Win32;
-using MPExtended.Libraries.Service.ConfigurationContracts;
-using MPExtended.Libraries.Service.Util;
-
-namespace MPExtended.Libraries.Service.Hosting
-{
-    public class Service
-    {
-        public MPExtendedService ServiceName { get; set; }
-        public string ImplementationName { get; set; }
-        public string Assembly { get; set; }
-        public string ZeroconfServiceType { get; set; }
-
-        public virtual bool HostAsWCF
-        {
-            get
-            {
-                return true;
-            }
-        }
-
-        public virtual bool IsInstalled
-        {
-            get
-            {
-                return Installation.GetFileLayoutType() == FileLayoutType.Source ||
-                    RegistryReader.ReadKey(RegistryHive.LocalMachine, @"Software\MPExtended", ServiceName.ToString() + "Installed") != null;
-            }
-        }
-
-        public virtual int Port
-        {
-            get
-            {
-                return Configuration.Services.Port;
-            }
-        }
-
-        public virtual string AssemblyPath
-        {
-            get
-            {
-                return Installation.GetFileLayoutType() == FileLayoutType.Source ? 
-                    Path.Combine(Installation.GetSourceRootDirectory(), "Services", Assembly, "bin", "Debug", Assembly + ".dll") :
-                    Path.Combine(Installation.GetInstallDirectory(MPExtendedProduct.Service), Assembly + ".dll");
-            }
-        }
-
-        public string FullTypeName
-        {
-            get
-            {
-                return Assembly + "." + ImplementationName;
-            }
-        }
-
-        public Service(MPExtendedService srv, string assembly, string implementationName, string serviceType)
-        {
-            this.ServiceName = srv;
-            this.Assembly = assembly;
-            this.ImplementationName = implementationName;
-            this.ZeroconfServiceType = serviceType;
-        }
-
-        public virtual void GetUsernameAndPassword(User user, out string username, out string password)
-        {
-            username = user.Username;
-            password = user.GetPassword();
-        }
-    }
-}

Libraries/MPExtended.Libraries.Service/Hosting/ServiceAssemblyAttribute.cs
@@ -1,0 +1,33 @@
+﻿#region Copyright (C) 2012 MPExtended
+// Copyright (C) 2012 MPExtended Developers, http://mpextended.github.com/
+// 
+// MPExtended is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MPExtended is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MPExtended. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MPExtended.Libraries.Service.Hosting
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true, Inherited = true)]
+    public class ServiceAssemblyAttribute : Attribute
+    {
+        public MPExtendedService Service { get; set; }
+        public Type WCFType { get; set; }
+        public Action BackgroundThread { get; set; }
+        public string ZeroconfType { get; set; }
+    }
+}

commit def1b5501739833a919ca4900b808a9cdc0e9c5c

Configurator: Also add WifiRemote to the QR-code
This was broken since commit 888c8bb. Add a trimmed down version of the
WifiRemoteService class back to parse the WifiRemote-configuration.

Applications/MPExtended.Applications.ServiceConfigurator/Pages/TabBarcode.xaml.cs
@@ -118,6 +118,7 @@ namespace MPExtended.Applications.ServiceConfigurator.Pages
                 desc.QRVersion = 1;
 
                 desc.Services = new List<ServiceDescription>();
+                User wifiRemoteAuth = WifiRemote.IsInstalled ? WifiRemote.GetAuthentication() : null;
                 foreach (var srv in Installation.GetInstalledServices())
                 {
                     var srvdesc = new ServiceDescription()
@@ -128,8 +129,8 @@ namespace MPExtended.Applications.ServiceConfigurator.Pages
 
                     if (auth != null)
                     {
-                        srvdesc.User = auth.Username;
-                        srvdesc.Password = auth.GetPassword();
+                        srvdesc.User = (srv.Service == MPExtendedService.WifiRemote ? wifiRemoteAuth : auth).Username;
+                        srvdesc.Password = (srv.Service == MPExtendedService.WifiRemote ? wifiRemoteAuth : auth).GetPassword();
                     }
 
                     desc.Services.Add(srvdesc);

Libraries/MPExtended.Libraries.Service/Installation.cs
@@ -257,12 +257,24 @@ namespace MPExtended.Libraries.Service
 
         public static List<ServiceConfiguration> GetInstalledServices()
         {
-            return GetAvailableServices().Select(x => new ServiceConfiguration()
+            var list = GetAvailableServices().Select(x => new ServiceConfiguration()
             {
                 Port = Configuration.Services.Port,
                 Service = x.Service,
                 ZeroconfType = x.ZeroconfType
             }).ToList();
+
+            if (WifiRemote.IsInstalled)
+            {
+                list.Add(new ServiceConfiguration()
+                {
+                    Port = WifiRemote.Port,
+                    Service = MPExtendedService.WifiRemote,
+                    ZeroconfType = WifiRemote.ZeroconfName
+                });
+            }
+
+            return list;
         }
 
         public static bool IsServiceInstalled(MPExtendedService srv)

Libraries/MPExtended.Libraries.Service/MPExtended.Libraries.Service.csproj
@@ -80,6 +80,7 @@
     <Compile Include="Hosting\ServiceState.cs" />
     <Compile Include="Util\CultureDatabase.cs" />
     <Compile Include="Util\StringExtensions.cs" />
+    <Compile Include="Util\WifiRemote.cs" />
     <Compile Include="WCFClient.cs" />
     <Compile Include="WCF\CustomQueryStringConverter.cs" />
     <Compile Include="Hosting\BaseAddresses.cs" />

Libraries/MPExtended.Libraries.Service/Util/WifiRemote.cs
@@ -1,0 +1,111 @@
+﻿#region Copyright (C) 2011-2012 MPExtended
+// Copyright (C) 2011-2012 MPExtended Developers, http://mpextended.github.com/
+// 
+// MPExtended is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MPExtended is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MPExtended. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+using MPExtended.Libraries.Service.ConfigurationContracts;
+
+namespace MPExtended.Libraries.Service.Util
+{
+    public class WifiRemote
+    {
+        public static bool IsInstalled
+        {
+            get
+            {
+                try
+                {
+                    if (Mediaportal.HasValidConfigFile())
+                    {
+                        var config = Mediaportal.ReadSectionFromConfigFile("plugins");
+                        return config.ContainsKey("WifiRemote") && config["WifiRemote"] == "yes";
+                    }
+
+                    return false;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static int Port
+        {
+            get
+            {
+                try
+                {
+                    string port = Mediaportal.ReadSectionFromConfigFile("WifiRemote")["port"];
+                    return Int32.Parse(port);
+                }
+                catch (Exception)
+                {
+                    return 8017;
+                }
+            }
+        }
+
+        public static string ZeroconfName
+        {
+            get
+            {
+                return "_mepo-remote._tcp";
+            }
+        }
+
+        public static User GetAuthentication()
+        {
+            User user = new User();
+
+            try
+            {
+                user.Username = DecryptConfig(Mediaportal.ReadSectionFromConfigFile("WifiRemote")["username"]);
+                user.SetPasswordFromPlaintext(DecryptConfig(Mediaportal.ReadSectionFromConfigFile("WifiRemote")["password"]));
+            }
+            catch (Exception)
+            {
+                user.Username = String.Empty;
+                user.SetPasswordFromPlaintext(String.Empty);
+            }
+
+            return user;
+        }
+
+        private static string DecryptConfig(string data)
+        {
+            // I don't really get why this is used at all, since it doesn't provide any security. The key is static and can be found in the MP sources, and
+            // 3DES isn't very secure anymore. Also, hashing is a lot better to use here as we never need the plaintext variant. Well, we've to deal with 
+            // it. More or less taken from mediaportal/Core/Util/EncryptDecrypt.cs in the MP sources, it's at least where the encoder resides, so we've to
+            // be compatible with it.
+
+            byte[] dataArray = Convert.FromBase64String(data);
+            var tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = UTF8Encoding.BigEndianUnicode.GetBytes("MPcrypto");
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+            var transform = tdes.CreateDecryptor();
+            byte[] resultArray = transform.TransformFinalBlock(dataArray, 0, dataArray.Length);
+            tdes.Clear();
+            return UTF8Encoding.BigEndianUnicode.GetString(resultArray);
+        }
+    }
+}