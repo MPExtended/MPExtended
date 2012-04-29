c8433195e13ff49cc696f492177df3dda9cd60d1
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2012-04-29 18:14:47 +0200

    Keep track of when the last message was received
    Expose this time through a client API in MetaService, so that clients can use
    it to decide whether the PC can be turned off.

--- Services/MPExtended.Services.MetaService.Interfaces/IProtectedMetaService.cs
+++ Services/MPExtended.Services.MetaService.Interfaces/IProtectedMetaService.cs
@@ -12,6 +12,6 @@ namespace MPExtended.Services.MetaService.Interfaces
     {
         [OperationContract]
         [WebGet(ResponseFormat = WebMessageFormat.Json)]
-        void DummyMethod();
+        DateTime GetLastClientActivity();
     }
 }
