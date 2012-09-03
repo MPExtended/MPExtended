fea89c6a5bb8045ca914cac1c5b96df9197c2839
diebagger <mail.bgmeiner@gmail.com>
2012-09-03 17:50:54 +0200

    Initial commit for internal-scrapers-v2 branch

---
+++ Services/MPExtended.Services.ScraperService.Interfaces/IScraperService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MPExtended.Services.ScraperService.Interfaces
+{
+    [ServiceContract(Namespace = "http://mpextended.github.com")]
+    public interface IScraperService
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        WebResult StartScraper();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        WebResult StopScraper();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        WebResult PauseScraper();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        WebResult ResumeScraper();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        WebScraperStatus GetScraperStatus();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        WebScraperInputRequest GetScraperInputRequest(int index);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        IList<WebScraperInputRequest> GetAllScraperInputRequests();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        WebResult SetScraperInputRequest(String requestId, String matchId, String text);
+    }
+}
---
+++ Services/MPExtended.Services.ScraperService.Interfaces/WebScraperStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MPExtended.Services.ScraperService.Interfaces
+{
+    public class WebScraperStatus
+    {
+        public String CurrentAction { get; set; }
+        public int CurrentProgress { get; set; }
+        public int InputNeeded { get; set; }
+        public WebScraperState ScraperState { get; set; }
+
+        public override string ToString()
+        {
+            return CurrentAction;
+        }
+    }
+}
