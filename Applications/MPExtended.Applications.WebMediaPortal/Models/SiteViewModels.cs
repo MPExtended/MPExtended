47d469919667bb9032ac78fe90033ee527821995
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2012-09-10 20:24:17 +0200

    WebMP: Technical fixes to the default skin
    - Remove unused files.
    - Move loading of genres to a ViewModel and Controller. This enables us to
      add caching for them later, and it's prettier.
    - Cosmetic changes to the source code.
    - Replace "No Image" text with "No image".
    - Use translated strings instead of hardcoded texts.
    - Add a bit of padding to the bottom of the dropdown menus.

---
+++ Applications/MPExtended.Applications.WebMediaPortal/Models/SiteViewModels.cs
@@ -0,0 +1,46 @@
+﻿#region Copyright (C) 2012 MPExtended
+// Copyright (C) 2012 MPExtended Developers, http://mpextended.github.com/
+// 
+// MPExtended is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MPExtended is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MPExtended. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MPExtended.Applications.WebMediaPortal.Code;
+
+namespace MPExtended.Applications.WebMediaPortal.Models
+{
+    public class MenuModel
+    {
+        public IEnumerable<string> MovieGenres
+        {
+            get
+            {
+                return Connections.Current.MAS.GetMovieGenres(Settings.ActiveSettings.MovieProvider)
+                    .Select(x => x.Title);
+            }
+        }
+
+        public IEnumerable<string> TVShowGenres
+        {
+            get
+            {
+                return Connections.Current.MAS.GetTVShowGenres(Settings.ActiveSettings.TVShowProvider)
+                    .Select(x => x.Title);
+            }
+        }
+    }
+}
