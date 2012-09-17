697cf6262e468d77f443769e4bbe699afdd6769f
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2012-09-17 18:14:43 +0200

    TAS: Fix indentation in WebChannelBasic

--- Services/MPExtended.Services.TVAccessService.Interfaces/WebChannelBasic.cs
+++ Services/MPExtended.Services.TVAccessService.Interfaces/WebChannelBasic.cs
@@ -5,11 +5,11 @@ using System.Text;
 
 namespace MPExtended.Services.TVAccessService.Interfaces
 {
-	public class WebChannelBasic
-	{
+    public class WebChannelBasic
+    {
         public string Title { get; set; }
         public int Id { get; set; }
         public bool IsRadio { get; set; }
         public bool IsTv { get; set; }
-	}
+    }
 }
