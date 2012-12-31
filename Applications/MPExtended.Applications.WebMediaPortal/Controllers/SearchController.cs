480470484c1f530c0f3d39f8991180580610a0d8
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2012-12-31 12:25:03 +0100

    WebMP: redirect to single search result (closes #356)
    Thanks to Ollienator for the code and Lyfesaver for the suggestion.

--- Applications/MPExtended.Applications.WebMediaPortal/Controllers/SearchController.cs
+++ Applications/MPExtended.Applications.WebMediaPortal/Controllers/SearchController.cs
@@ -53,8 +53,8 @@ namespace MPExtended.Applications.WebMediaPortal.Controllers
 
             list = list.Where(x => x.URL != null).OrderByDescending(x => x.Score);
 
-            // when there is *only* one hit with a 100% score, just redirect to that page
-            if(list.Where(x => x.Score == 100).Count() == 1)
+            // when there is exactly one hit, or *only* one with hit with a 100% score, just redirect to that page
+            if(list.Count == 1 || list.Count(x => x.Score == 100) == 1)
             {
                 return Redirect(list.First().URL);
             }
