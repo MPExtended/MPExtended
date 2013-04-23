9d21f978673d931938b46e3a3ca55b78339b8bf1
Benjamin Gmeiner <bgmeiner@gmail.com>
2013-04-23 17:46:32 +0200

    Updated copyright header

--- Applications/MPExtended.Applications.WebMediaPortal/Code/ActionResults/RangeFileResult.cs
+++ Applications/MPExtended.Applications.WebMediaPortal/Code/ActionResults/RangeFileResult.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿#region Copyright (C) 2013 TPeczek, 2013 MPExtended (Ms-PL)
+// Copyright (C) 2013 MPExtended Developers, http://mpextended.github.com/
+// Copyright (C) 2013 TPeczek, http://tpeczek.codeplex.com/
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +34,6 @@ namespace MPExtended.Applications.WebMediaPortal.Code.ActionResults
 {
     /// <summary>
     /// Represents a base class that is used to send binary file content to the range response.
-    /// 
-    /// Original code: http://tpeczek.codeplex.com/
     /// </summary>
     public abstract class RangeFileResult : ActionResult
     {
