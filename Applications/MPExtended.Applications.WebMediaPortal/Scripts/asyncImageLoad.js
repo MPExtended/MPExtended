e970ac23f3dde6a9cf48d427c22dfd534d47602f
jpykie <jbpdev@googlemail.com>
2013-07-08 14:10:48 +0100

    Client side pagination and UI tweak - additional files

---
+++ Applications/MPExtended.Applications.WebMediaPortal/Scripts/asyncImageLoad.js
@@ -0,0 +1,32 @@
+﻿
+function LoadAllImages()
+{
+    LoadImageAsync($($('.async-image-display').get(0)));
+}
+
+function LoadImageAsync(imageContainer)
+{
+    var img = new Image();
+
+    // Set the new image up
+    $(img)
+        // Do this before attaching to the load event
+        .css('height', imageContainer.css('height'))
+        .css('width', imageContainer.css('width'))
+        .attr('alt', imageContainer.attr('data-alt'))
+        // once the image has loaded, execute this code
+        .load(function () {
+            $(this).hide();
+            // remove the loading class (so no background spinner), 
+            imageContainer.removeClass('async-image-display')
+            // then insert our image
+            imageContainer.append(this);
+
+            $(this).fadeIn();
+
+            if ($('.async-image-display').get(0) != null)
+                LoadImageAsync($($('.async-image-display').get(0)));
+        })
+        // Set the src attribute, which will fire load when complete
+        .attr('src', imageContainer.attr('data-src'));
+}
