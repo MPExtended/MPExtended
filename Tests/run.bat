b47f41a2b8fec5462adcda9004c293ebf938ec4c
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2013-10-18 00:06:32 +0200

    Tests: Fix references to old xunit version.

--- Tests/run.bat
+++ Tests/run.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 if not exist output ( mkdir output )
-..\Packages\xunit.runners.1.9.1\tools\xunit.console.clr4.x86 .\mpextended.xunit
+..\Packages\xunit.runners.1.9.2\tools\xunit.console.clr4.x86 .\mpextended.xunit
 pause>nul
