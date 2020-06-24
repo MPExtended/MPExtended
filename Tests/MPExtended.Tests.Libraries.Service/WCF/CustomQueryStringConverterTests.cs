d757276eedaea1c453f65f464f99cc9c7b3d5675
Andrew J.Swan <ajs.blackhole@gmail.com>
2020-06-24 10:51:57 +0300

    Add TZ to Date converter test

--- Tests/MPExtended.Tests.Libraries.Service/WCF/CustomQueryStringConverterTests.cs
+++ Tests/MPExtended.Tests.Libraries.Service/WCF/CustomQueryStringConverterTests.cs
@@ -49,8 +49,8 @@ namespace MPExtended.Tests.Libraries.Service.WCF
 
             Assert.Equal(new DateTime(2012, 8, 7, 15, 00, 00, DateTimeKind.Utc).ToLocalTime(), converter.ConvertStringToValue("2012-08-07T15:00:00Z", typeof(DateTime)));
             Assert.Equal(new DateTime(2012, 8, 7, 0, 0, 0, DateTimeKind.Local), converter.ConvertStringToValue("2012-08-07", typeof(DateTime)));
-            // TODO: test for timezone magic here
-            Assert.Equal(new DateTime(2012, 1, 17, 14, 52, 29, DateTimeKind.Utc).ToLocalTime(), converter.ConvertStringToValue("/Date(1326815549000+0000)/", typeof(DateTime)));
+            // TODO: test for timezone magic here and maybe for Daylight
+            Assert.Equal(new DateTime(2012, 1, 17, 14, 52, 29, DateTimeKind.Utc).ToLocalTime(), converter.ConvertStringToValue("/Date(1326815549000-1000)/", typeof(DateTime)));
         }
     }
 }
