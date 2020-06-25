f14797ebcf693d9c614a6f1aa11ab9c701097dcd
Andrew J.Swan <ajs.blackhole@gmail.com>
2020-06-25 14:29:32 +0300

    Add GetAllCollections to MyVideo

--- PlugIns/MPExtended.PlugIns.MAS.MPVideos/MPVideos.cs
+++ PlugIns/MPExtended.PlugIns.MAS.MPVideos/MPVideos.cs
@@ -214,7 +214,6 @@ namespace MPExtended.PlugIns.MAS.MPVideos
                     }
                   }
                 }
-
               }
               return item;
             });
@@ -333,11 +332,90 @@ namespace MPExtended.PlugIns.MAS.MPVideos
 
         public IEnumerable<WebCategory> GetAllCategories()
         {
-            string sql = "SELECT strGroup, strGroupDescription  FROM usergroup WHERE idGroup in (SELECT idGroup FROM usergrouplinkmovie)";
+            string sql = "SELECT strGroup, strGroupDescription FROM usergroup WHERE idGroup in (SELECT idGroup FROM usergrouplinkmovie)";
             return new LazyQuery<WebCategory>(this, sql, new List<SQLFieldMapping>()
             {
                 new SQLFieldMapping("strGenre", "Title", DataReaders.ReadString),
                 new SQLFieldMapping("strGroupDescription", "Description", DataReaders.ReadString)
+            });
+        }
+
+        public IEnumerable<WebCategory> GetAllCollections()
+        {
+            string sql = "SELECT strCollection FROM moviecollection WHERE idCollection in (SELECT idCollection FROM moviecollectionlinkmovie)";
+            return new LazyQuery<WebCollection>(this, sql, new List<SQLFieldMapping>()
+            {
+                new SQLFieldMapping("strCollection", "Title", DataReaders.ReadString)
+            }, delegate (T item)
+            {
+              if (item is WebCollection)
+              {
+                // Poster
+                int i = 0;
+                var files = new string[] {
+                        PathUtil.StripInvalidCharacters(string.Format("{0}{1}", item.Title, "L.jpg"), '_'),
+                        PathUtil.StripInvalidCharacters(string.Format("{0}{1}", item.Title, ".jpg"), '_')
+                }
+                  .Select(x => Path.Combine(configuration["cover"], "Collection", x))
+                  .Where(x => File.Exists(x))
+                  .Distinct();
+                if (files != null && files.Count() > 0)
+                {
+                  item.Artwork.Clear();
+                }
+                foreach (string file in files)
+                {
+                  item.Artwork.Add(new WebArtworkDetailed()
+                  {
+                    Type = WebFileType.Cover,
+                    Offset = i++,
+                    Path = file,
+                    Rating = 1,
+                    Id = file.GetHashCode().ToString(),
+                    Filetype = Path.GetExtension(file).Substring(1)
+                  });
+                }
+                // Backdrops
+                i = 0;
+                string thumbfolder = Path.Combine(fanartconfiguration["thumb"], "Skin Fanart", "Scraper", "Movies");
+                if (Directory.Exists(thumbfolder))
+                {
+                  files = Directory.GetFiles(thumbfolder, string.Format("{0}{{*}}.jpg", item.Title))
+                    .Where(x => File.Exists(x))
+                    .Distinct();
+                  foreach (string file in files)
+                  {
+                    item.Artwork.Add(new WebArtworkDetailed()
+                    {
+                      Type = WebFileType.Backdrop,
+                      Offset = i++,
+                      Path = file,
+                      Rating = 1,
+                      Id = file.GetHashCode().ToString(),
+                      Filetype = Path.GetExtension(file).Substring(1)
+                    });
+                  }
+                }
+                // ClearArt
+                string logofolder = Path.Combine(fanartconfiguration["thumb"], "ClearArt", "MoviesCollections");
+                if (Directory.Exists(thumbfolder))
+                {
+                    string file = Path.Combine(logofolder, string.Format("{0}.png", item.Title));
+                    if (File.Exists(file))
+                    {
+                      item.Artwork.Add(new WebArtworkDetailed()
+                      {
+                        Type = WebFileType.Logo,
+                        Offset = 0,
+                        Path = file,
+                        Rating = 1,
+                        Id = file.GetHashCode().ToString(),
+                        Filetype = Path.GetExtension(file).Substring(1)
+                      });
+                    }
+                }
+              }
+              return item;
             });
         }
 
