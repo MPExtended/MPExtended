4fe3900925571a8a9ecc00a8773a6617a9770538
Andrew J.Swan <ajs.blackhole@gmail.com>
2020-07-06 22:28:32 +0300

    Add WebGeoLocation

---
+++ Services/MPExtended.Services.StreamingService.Interfaces/WebGeoLocation.cs
@@ -0,0 +1,38 @@
+﻿#region Copyright (C) 2020 Team MediaPortal
+// Copyright (C) 2020 Team MediaPortal, http://www.team-mediaportal.com/
+// 
+// MPExtended is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MPExtended is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MPExtended. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System.Globalization;
+
+namespace MPExtended.Services.StreamingService.Interfaces
+{
+  public class WebGeoLocation
+  {
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double Altitude { get; set; }
+
+    public bool IsZero()
+    {
+      return (Latitude + Longitude + Altitude) == 0;
+    }
+
+    public override string ToString()
+    {
+      return Latitude.ToString(CultureInfo.InvariantCulture) + "," + Longitude.ToString(CultureInfo.InvariantCulture);
+    }
+  }
+}
