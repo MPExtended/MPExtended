9eaf84817cbe860189abfdbf9b1b556bd2622494
Andrew J.Swan <ajs.blackhole@gmail.com>
2020-07-17 09:51:52 +0300

    Add WebMobileVideoBasic

---
+++ Services/MPExtended.Services.MediaAccessService.Interfaces/Picture/WebMobileVideoBasic.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MPExtended.Services.Common.Interfaces;
+
+namespace MPExtended.Services.MediaAccessService.Interfaces.Picture
+{
+    public class WebPictureBasic : WebMediaItem, IDateAddedSortable, IPictureDateTakenSortable, ICategorySortable
+    {
+        public WebPictureBasic()
+        {
+            DateTaken = new DateTime(1970, 1, 1);
+            Categories = new List<WebCategory>();
+        }
+
+        public IList<WebCategory> Categories { get; set; }
+        public DateTime DateTaken { get; set; }
+        public string Description { get; set; }
+
+        public override WebMediaType Type
+        {
+            get
+            {
+                return WebMediaType.MobileVideo;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
