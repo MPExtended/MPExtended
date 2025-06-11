0e7abc57cf37cfe61056a2fcd718637da0024d39
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-06-11 11:21:28 +0300

    Bump Windows to Windows Server 2022

--- Tools/restore-packages-and-log-output.bat
+++ Tools/restore-packages-and-log-output.bat
@@ -2,8 +2,21 @@
 
 echo Please wait while packages are restored.
 
+FOR %%p IN ("%PROGRAMFILES(x86)%" "%PROGRAMFILES%") DO (
+  FOR %%s IN (2019 2022) DO (
+    FOR %%e IN (Community Professional Enterprise BuildTools) DO (
+      SET PF=%%p
+      SET PF=!PF:"=!
+      SET MSBUILD_PATH="!PF!\Microsoft Visual Studio\%%s\%%e\MSBuild\Current\Bin\MSBuild.exe"
+      IF EXIST "!MSBUILD_PATH!" GOTO :BUILD
+    )
+  )
+)
+
+:BUILD
+
 echo ^>^> Package restore started in %CD% at %DATE% %TIME% > restore-packages.log
 echo. >> restore-packages.log
-@"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" restore-packages.targets >> restore-packages.log
+%MSBUILD_PATH% restore-packages.targets >> restore-packages.log
 echo. >> restore-packages.log
 echo ^>^> Package restore finished at %DATE% %TIME% >> restore-packages.log
